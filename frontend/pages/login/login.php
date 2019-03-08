b2483c126170c106a0345781544c5c1b07f70d21
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-03-07 18:54:48 -0800

    Update login

---
+++ frontend/pages/login/login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="login.css">
+  <title>Login Page</title>
+</head>
+
+<body>
+
+<main>
+  <div id="main-left">
+    <div id="login-brand">
+      <p>Onlive Virus Checker</p>
+    </div>
+    <div id="login-container">
+      <h2>Welcome back, please login to your account</h2>
+      <input type="email">
+      <p>Sorry, your username must be between 6 and 30 characters long.</p>
+      <p>Sorry, only letters (a-z), numbers (0-9), and periods (.) are allowed.</p>
+      <input type="password">
+      <p>Your password can't start or end with a blank space</p>
+      <input type="submit" value="Login">
+    </div>
+    <div id="login-credit">
+      <p>Nhat Jasmine Albert</p>
+    </div>
+  </div>
+  <div id="main-right"></div>
+</main>
+
+</body>
+
+</html>
