26ca3e575abe73a8f675afe2f4b40e44c30f4cf2
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-03-21 13:18:03 -0700

    Use components

--- frontend/pages/login/login.php
+++ frontend/pages/login/login.php
@@ -12,19 +12,13 @@
       
       <form>
 
-      <!-- <p>Sorry, your username must be between 6 and 30 characters long.</p>
-      <p>Sorry, only letters (a-z), numbers (0-9), and periods (.) are allowed.</p> -->
-      <label for="email">Email</label>
-      <input type="email" name="email" placeholder="e.g. johndoe@gmail.com">
+        <?php include '../../components/input-email.php'; ?>
+        <?php include '../../components/input-password.php'; ?>
 
-      <!-- <p>Your password can't start or end with a blank space</p> -->
-      <label for="password">Password</label>
-      <input type="password" name="password" placeholder="Must be at least 6 characters">
-
-      <div id="spaceBetween">
-        <a id="signupLink" href="http://localhost/cs174/online-virus-check/frontend/pages/signup/base.php">Create account</a>
-        <input type="submit" value="Login">
-      </div>
+        <div id="spaceBetween">
+          <a id="signupLink" href="http://localhost/cs174/online-virus-check/frontend/pages/signup/base.php">Create account</a>
+          <input type="submit" value="Login">
+        </div>
 
       </form>
     </div>
--- frontend/pages/signup/signup.php
+++ frontend/pages/signup/signup.php
@@ -10,26 +10,19 @@
       <div id="login-container">
         <h2>Create your account</h2>
 
-        <form>
+          <form>
 
-          <label for="name">Name</label>
-          <input type="text" name="name" placeholder="e.g. John Doe">
+            <?php include '../../components/input-name.php'; ?>
+            <?php include '../../components/input-email.php'; ?>
+            <?php include '../../components/input-password.php'; ?>
 
-          <!-- <p>Sorry, your username must be between 6 and 30 characters long.</p>
-      <p>Sorry, only letters (a-z), numbers (0-9), and periods (.) are allowed.</p> -->
-          <label for="email">Email</label>
-          <input type="email" name="email" placeholder="e.g. johndoe@gmail.com">
+            <div id="spaceBetween">
+              <a id="signupLink" href="http://localhost/cs174/online-virus-check/frontend/pages/login/base.php">Log in instead</a>
+              <input type="submit" value="Signup">
+            </div>
 
-          <!-- <p>Your password can't start or end with a blank space</p> -->
-          <label for="password">Password</label>
-          <input type="password" name="password" placeholder="Must be at least 6 characters">
-
-          <div id="spaceBetween">
-            <a id="signupLink" href="http://localhost/cs174/online-virus-check/frontend/pages/login/base.php">Log in instead</a>
-            <input type="submit" value="Signup">
-          </div>
-
-        </form>
+          </form>
+          
       </div>
 
       <div id="login-credit">
