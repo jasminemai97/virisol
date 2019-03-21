69c32b05ee11cfea40846a4bc94f7c5b192084fb
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-03-21 12:48:08 -0700

    Update Login

--- frontend/pages/login/login.php
+++ frontend/pages/login/login.php
@@ -14,32 +14,34 @@
 <main>
   <div id="main-left">
     <div id="login-brand">
-      <p id="title">CS174 - Onlive Virus Checker</p>
+      <p id="title">CS174 • Onlive Virus Checker</p>
     </div>
 
-    <!-- Form Start -->
+    <!-- Form Start ================================== -->
     <div id="login-container">
       <h2>Welcome back, please login to your account</h2>
       
       <form>
-      
-      
+
       <!-- <p>Sorry, your username must be between 6 and 30 characters long.</p>
       <p>Sorry, only letters (a-z), numbers (0-9), and periods (.) are allowed.</p> -->
       <label for="email">Email</label>
-      <input type="email" name="email" placeholder="e.g. John Cena">
+      <input type="email" name="email" placeholder="e.g. johndoe@gmail.com">
 
       <!-- <p>Your password can't start or end with a blank space</p> -->
       <label for="password">Password</label>
       <input type="password" name="password" placeholder="Must be at least 6 characters">
 
-      <input type="submit" value="Login">
-      
+      <div id="spaceBetween">
+        <a id="signupLink" href="#">Create account</a>
+        <input type="submit" value="Login">
+      </div>
+
       </form>
     </div>
     
     <div id="login-credit">
-      <p>Nhat Jasmine Albert</p>
+      <p>Nhat Nguyen • Jasmine Mai • Albert Ong</p>
     </div>
   </div>
   <div id="main-right"></div>
