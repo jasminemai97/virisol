49a9e25c3f5338455df26495c3bd4b61a97ea1c1
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-03-22 00:14:41 -0700

    Example PHP to MySQL connection

---
+++ example.php
@@ -0,0 +1,17 @@
+<?php 
+
+  // Information needed to connect to the MySQL database. 
+  $server_name = "localhost"
+  $database_username = "root";
+  $database_password = "";
+  $database_name = "cs174_db";
+
+  // Connects to the MySQL database. 
+  $conn = new mysqli($server_name, $database_username, $database_password, $database_name);
+
+
+  // Error message if unable to connect. 
+  if ($conn -> connect_error) {
+    die("Unable to connect");
+  }
+?>
