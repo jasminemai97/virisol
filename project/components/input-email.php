bef5f1d2cc849667d5c0b9d96741ded7853018a4
Anferensis <anferensis@gmail.com>
2019-04-03 18:07:54 -0700

    Add files via upload

--- project/components/input-email.php
+++ project/components/input-email.php
@@ -1,4 +1,19 @@
+
+<!--
+input-email.php
+
+Allows the user to input their email.
+Used in both login.php and signup.php. 
+
+Online Virus Check
+CS 174: Server-side Web Programming
+Professor Fabio Di Troia
+
+Written by Jasmine Mai, Nhat Nguyen, and Albert Ong
+Revision 03.04.2019
+-->
+
 <!-- <p>Sorry, your username must be between 6 and 30 characters long.</p>
 <p>Sorry, only letters (a-z), numbers (0-9), and periods (.) are allowed.</p> -->
-<label for="email">Email <span>*</span></label>
+<label for="email" class="component-title">Email <span>*</span></label>
 <input type="email" name="email" placeholder="e.g. johndoe@gmail.com" spellcheck="false" required>
