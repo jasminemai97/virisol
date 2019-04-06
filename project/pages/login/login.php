a9ac1e8731cdbb6fa3c8582b0680bdb1cce607d0
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-04-05 17:21:45 -0700

    Sprint 04.05.2019

--- project/pages/login/login.php
+++ project/pages/login/login.php
@@ -2,7 +2,7 @@
 <!--
 login.php
 
-The login page for Online Virus check. 
+The login page for Online Virus check.
 
 Online Virus Check
 CS 174: Server-side Web Programming
@@ -21,59 +21,59 @@ Revision 03.04.2019
     <!-- Form Start ================================== -->
     <div id="login-container">
       <h2>Login to your account</h2>
-      
+
       <form method="POST">
-          
+
         <!-- Email and password inputs -->
         <?php require '../../components/input-email.php'; ?>
         <?php require '../../components/input-password.php'; ?>
-        
+
         <!-- Create account and login buttons -->
         <div id="spaceBetween">
           <a class="center" id="signupLink" href="../signup/">Create account</a>
           <input type="submit" name="submit-login" value="Login">
         </div>
       </form>
-      
-      
+
+
       <?php
-        // Retrieves the inputted email and password. 
+        // Retrieves the inputted email and password.
         $email_input = isset($_POST["email"]) ? $_POST["email"] : null;
         $password_input = isset($_POST["password"]) ? $_POST["password"] : null;
-       
-        // Loads the user database from MySQL. 
-        require '../../scripts/connection.php'; 
+
+        // Loads the user database from MySQL.
+        require '../../scripts/connection.php';
         $table_data = $conn -> query("SELECT email, password FROM users");
 
-        $login_success = false; 
-       
-        // Iterates through every row in the user database. 
+        $login_success = false;
+
+        // Iterates through every row in the user database.
         while ($row = $table_data -> fetch_assoc()) {
           $email = $row["email"];
           $password = $row["password"];
-          
-          // If the email and password match a registered user, the login is successful. 
+
+          // If the email and password match a registered user, the login is successful.
           if ($email_input == $email && $password_input == $password) {
             $login_success = true;
             break;
           }
         }
-      
-        // Prints out a message if the login was successful or not. 
-//        if ($login_success) {
-//          echo "Login successful!";
-//        }
-//        else {
-//          echo "Login failed. ";
-//        }
+
+        // Prints out a message if the login was successful or not.
+        // if ($login_success) {
+        //   echo "Login successful!";
+        // }
+        // else {
+        //   echo "Login failed. ";
+        // }
       ?>
     </div>
-    
+
     <!-- Authors text -->
     <div id="login-credit">
       <p id="authors">Authors: Jasmine Mai, Nhat Nguyen, and Albert Ong</p>
     </div>
-    
+
   </div>
   <div id="main-right"></div>
 </main>
--- project/scripts/connection.php
+++ project/scripts/connection.php
@@ -2,7 +2,7 @@
 <!--
 connection.php
 
-Connects the program to MySQL. 
+Connects the program to MySQL.
 
 Online Virus Check
 CS 174: Server-side Web Programming
@@ -18,40 +18,42 @@ Revision 03.04.2019
   $hostname = "localhost";
   $username = "root";
   $password = "";
-  $database = "online_virus_check_database";
+  $database = "project_db";
 
   // Connects to the MySQL database. Error message if unable to connect.
   $conn = new mysqli($hostname, $username, $password, $database);
 
   if ($conn->connect_error) {
+    echo "The database does not exist. <br>";
     die($conn->connect_error);
   }
 
   // echo "Connected to MySQL successfully! <br>";
 
-  // Checks if a table named 'users' exists. 
+  // Checks if a table named 'users' exists.
   $tables = $conn -> query("SHOW TABLES LIKE 'users'");
-  $table_exists = $tables -> num_rows == 1; 
+  $table_exists = $tables -> num_rows == 1;
 
-  // If the table 'users' does not exist... 
+  // If the table 'users' does not exist...
   if (!$table_exists) {
     echo "The table does not exist. ";
-    
-    // Initializes the table of users. 
-    $users_table = "CREATE TABLE users (
-      username VARCHAR(128), 
-      email VARCHAR(128), 
-      password VARCHAR(128), 
-      permission VARCHAR(32) 
-    )";
 
-    // Adds the three authors as admins. 
+    // Initializes the table of users.
+    $users_table = "CREATE TABLE users (
+      username VARCHAR(128),
+      email VARCHAR(128),
+      password VARCHAR(128),
+      isAdmin VARCHAR(1)
+    )
+    ENGINE MyISAM";
+
+    // Adds the three authors as admins.
     $user_data = "INSERT INTO users
-                    (username, email, password, permission)
-                  VALUES 
-                    ('Jasmine Mai', 'jasmine.mai@gmail.com', 'jasminemai', 'admin'), 
-                    ('Nhat Nguyen', 'nhat.nguyen@gmail.com', 'nhatnguyen', 'admin'), 
-                    ('Albert Ong', 'albert.ong@gmail.com', 'albertong', 'admin') ";
+                    (username, email, password, isAdmin)
+                  VALUES
+                    ('jasmine.mai@gmail.com', 'jasmine.mai@gmail.com', 'jasminemai', 'Y'),
+                    ('nhat.nguyen@gmail.com', 'nhat.nguyen@gmail.com', 'nhatnguyen', 'Y'),
+                    ('albert.ong@gmail.com', 'albert.ong@gmail.com', 'albertong', 'Y') ";
 
     $conn -> query($users_table);
     $conn -> query($user_data);
