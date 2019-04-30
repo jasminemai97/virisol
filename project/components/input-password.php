a7c0b31b2029c840fc9845175f0adba39d2de52d
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-04-29 21:45:01 -0700

    Refactor correct PHP format

--- project/components/input-password.php
+++ project/components/input-password.php
@@ -1,19 +1,8 @@
+<?php
 
-<!--
-input-password.php
-
-Allows the user to input their password.
-Used in both login.php and signup.php. 
-
-Online Virus Check
-CS 174: Server-side Web Programming
-Professor Fabio Di Troia
-
-Written by Jasmine Mai, Nhat Nguyen, and Albert Ong
-Revision 03.04.2019
--->
-
-<!-- https://support.google.com/a/answer/139399?hl=en -->
-<!-- <p>Your password can't start or end with a blank space</p> -->
+echo <<<_END
 <label for="password" class="component-title">Password <span>*</span></label>
 <input type="password" name="password" placeholder="Must be at least 6 characters" minlength="6" maxlength="100" required>
+_END;
+
+?>
--- project/components/input-username.php
+++ project/components/input-username.php
@@ -1,24 +1,19 @@
+<?php
 
-<!--
-input-name.php
+// Sorry, your username must be between 6 and 30 characters long.
+// Sorry, only letters (a-z), numbers (0-9), and periods (.) are allowed.
 
-Allows the user to input their name.
-Used in signup.php.
-
-Online Virus Check
-CS 174: Server-side Web Programming
-Professor Fabio Di Troia
-
-Written by Jasmine Mai, Nhat Nguyen, and Albert Ong
-Revision 03.04.2019
--->
-
+echo <<<_END
 <label for="username" class="component-title">Username <span>*</span></label>
 <input
-type="text"
-name="username"
-placeholder="e.g. BooksLover"
-spellcheck="false"
-pattern="[0-9a-zA-Z-_]+"
-title="The username can contain English letters (capitalized or not), digits, and the characters '_' (underscore) and '-' (dash)"
-required>
+  type="text"
+  name="username"
+  placeholder="e.g. BooksLover"
+  spellcheck="false"
+  pattern="[0-9a-zA-Z-_]+"
+  title="The username can contain English letters (capitalized or not), digits, and the characters '_' (underscore) and '-' (dash)"
+  required
+>
+_END;
+
+?>
