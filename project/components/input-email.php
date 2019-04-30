2233e6547da6fb9c40915d35d92cf6c4a1f18b77
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-04-29 23:55:12 -0700

    Gender neutral name

--- project/components/input-email.php
+++ project/components/input-email.php
@@ -2,7 +2,7 @@
 
 echo <<<_END
 <label for="email" class="component-title">Email <span>*</span></label>
-<input type="email" name="email" placeholder="e.g. emersondoe@gmail.com" spellcheck="false" required>
+<input type="email" name="email" placeholder="e.g. emersonnguyen@gmail.com" spellcheck="false" required>
 _END;
 
 ?>
