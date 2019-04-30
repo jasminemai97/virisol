ac25daa5ae4f364638554bf5483c0deb2b3e47d2
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-04-30 00:55:27 -0700

    Add new pages

---
+++ project/pages/admin-dashboard/index.php
@@ -0,0 +1,36 @@
+<?php
+
+// Start the project session
+session_start();
+
+// Database for user credentials
+require_once '../../scripts/credentialsDB.php';
+
+echo <<<_END
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="signup.css">
+  <link rel="icon" href="../../resources/favicon.png">
+  <title>Admin Dashboard - Online Virus Check</title>
+</head>
+
+<body>
+_END;
+
+  require 'signup.php';
+
+echo <<<_END
+</body>
+
+</html>
+_END;
+
+// Close the connection
+$conn->close();
+
+?>
