c108f4b65666b9601b18859873fdee1fb0b9cb12
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-04-30 00:57:38 -0700

    Minor tweaks

--- project/index.php
+++ project/index.php
@@ -9,8 +9,8 @@
  * Written by Jasmine Mai, Nhat Nguyen, and Albert Ong
  */
 
-    // First page is the signup page
-    header('Location: ./pages/user-signup/');
-    exit();
+// First page is the signup page
+header('Location: ./pages/user-signup/');
+exit();
 
 ?>
