35830f08ef5ba002858879b6019aea7550a4b0b3
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-04-30 00:13:18 -0700

    Fix error

--- project/components/message-signup.php
+++ project/components/message-signup.php
@@ -1,21 +1,9 @@
 <?php
 
-// Displays account success message
-if (isset($_SESSION["account_creation_successful"])) {
-  echo "<div class='message' id='green'>Account successful created!</div>";
-  unset($_SESSION["account_creation_successful"]);
-}
-
-// Displays login fail message
-if (isset($_SESSION["login_failed"])) {
-  echo "<div class='message' id='red'>Your email or password were incorrect!</div>";
-  unset($_SESSION["login_failed"]);
-}
-
-// Displays login success message
-if (isset($_SESSION["login_successful"])) {
-  echo "<div class='message' id='green'>You are successful logged in!</div>";
-  unset($_SESSION["login_successful"]);
+// Displays error message
+if (isset($_SESSION["account_creation_failed"])) {
+  echo "<div class='message' id='red'>Email or username already exist!</div>";
+  unset($_SESSION["account_creation_failed"]);
 }
 
 ?>
