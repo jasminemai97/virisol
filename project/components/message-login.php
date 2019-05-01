ff5cd110965d9fccdbd8c635af51d01865df7a73
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-05-01 02:58:18 -0700

    Update user form messages

--- project/components/message-login.php
+++ project/components/message-login.php
@@ -2,8 +2,14 @@
 
 // Displays account success message
 if (isset($_SESSION["account_creation_successful"])) {
-  echo "<div class='message' id='green'>Account successful created!</div>";
+  echo "<div class='message' id='green'>Account successfully created!</div>";
   unset($_SESSION["account_creation_successful"]);
+}
+
+// Displays login success message
+if (isset($_SESSION["login_successful"])) {
+  echo "<div class='message' id='green'>Account successfully logged out!</div>";
+  unset($_SESSION["login_successful"]);
 }
 
 // Displays login fail message
@@ -11,11 +17,5 @@ if (isset($_SESSION["login_failed"])) {
   echo "<div class='message' id='red'>Your email or password were incorrect!</div>";
   unset($_SESSION["login_failed"]);
 }
-
-// // Displays login success message
-// if (isset($_SESSION["login_successful"])) {
-//   echo "<div class='message' id='green'>You are successful logged in!</div>";
-//   unset($_SESSION["login_successful"]);
-// }
 
 ?>
