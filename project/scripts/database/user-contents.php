360ddd5afbdbcbed4d6bd4856bb31cb8456ea07d
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-05-01 02:49:39 -0700

    Update database for user and admin

--- project/scripts/database/user-contents.php
+++ project/scripts/database/user-contents.php
@@ -7,7 +7,7 @@ require_once "connection.php";
 require_once "database.php";
 
 // Declare table name
-$table_name = "admin_credentials";
+$table_name = "user_contents";
 
 // Checks if credentials table exists.
 $table_query = $conn -> query("SHOW TABLES LIKE '$table_name'");
@@ -19,9 +19,10 @@ if (!$table_exist) {
   // Declare MySQL table columns with data types
   $table = "CREATE TABLE $table_name (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    user_email VARCHAR(128) NOT NULL UNIQUE,
-    user_username VARCHAR(128) NOT NULL UNIQUE,
-    user_password VARCHAR(128) NOT NULL
+    user_email VARCHAR(128) NOT NULL,
+    user_filename VARCHAR(128) NOT NULL,
+    user_filecontent TEXT(65535) NOT NULL,
+    time_created TIMESTAMP NOT NULL
   ) ENGINE MyISAM";
 
   $conn -> query($table);
