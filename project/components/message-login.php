3dbf787d862ef3ff06be4a0b42c84f53556e7cf9
Nhat Nguyen <31426677+nguyen-nhat@users.noreply.github.com>
2019-05-01 04:42:11 -0700

    Update message logic

--- project/components/message-login.php
+++ project/components/message-login.php
@@ -3,19 +3,19 @@
 // Displays account success message
 if (isset($_SESSION["account_creation_successful"])) {
   echo "<div class='message' id='green'>Account successfully created!</div>";
-  unset($_SESSION["account_creation_successful"]);
 }
 
 // Displays login success message
 if (isset($_SESSION["login_successful"])) {
   echo "<div class='message' id='green'>Account successfully logged out!</div>";
-  unset($_SESSION["login_successful"]);
 }
 
 // Displays login fail message
-if (isset($_SESSION["login_failed"])) {
+if (isset($_SESSION["login_failed"]) || isset($_SESSION["admin_login_failed"])) {
   echo "<div class='message' id='red'>Your email or password were incorrect!</div>";
-  unset($_SESSION["login_failed"]);
 }
+
+// Free all session variables
+session_unset();
 
 ?>
--- project/components/message-signup.php
+++ project/components/message-signup.php
@@ -3,7 +3,9 @@
 // Displays error message
 if (isset($_SESSION["account_creation_failed"])) {
   echo "<div class='message' id='red'>Email or username already exist!</div>";
-  unset($_SESSION["account_creation_failed"]);
 }
+
+// Free all session variables
+session_unset();
 
 ?>
